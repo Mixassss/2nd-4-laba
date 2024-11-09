5c0196b7b4f5fd20ce4aa714c099695c85666a9e
Mixassss <bigboommaximandsim@gmail.com>
2024-11-09 16:00:11 +0700

    Error with barier

--- zad1.cpp
+++ zad1.cpp
@@ -1,4 +1,19 @@
-#include "utility.h"
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <barrier>
+#include <chrono>
+#include <random>
+#include <semaphore>
+
+using namespace std;
+
+const int thread_count = 10; // Кол-во потоков
+const int char_count = 5; // Кол-во символов, которое генеритруется потоком
+
+mutex mtx;
+counting_semaphore<1> sem(1);
+barrier<> my_barrier(thread_count);
 
 void generateRandomCharsMutex(int thread_id) {
     random_device rd;
@@ -8,19 +23,20 @@ void generateRandomCharsMutex(int thread_id) {
     for (int i = 0; i < char_count; ++i) {
         char random_char = dis(gen);
         lock_guard<mutex> lock(mtx);
-        cout << "Thread " << thread_id << ": " << random_char << endl;
+        cout << "Mutex thread " << thread_id << ": " << random_char << endl;
     }
 }
 
 void generateRandomCharsSemaphore(int thread_id) {
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(32,126); // Генерация случайных символов из ASII таблицы
+    uniform_int_distribution<> dis(32, 126);
 
     for(int i = 0; i < char_count; ++i) {
+        sem.acquire();
         char random_char = dis(gen);
-        sem_wait(&sem);
-        cout << "Thread " << thread_id << ": " << random_char << endl;
+        cout << "Semaphore thread " << thread_id << ": " << random_char << endl;
+        sem.release();
     }
 }
 
@@ -29,5 +45,31 @@ void generateRandomCharsBarrier(int thread_id) {
     mt19937 gen(rd());
     uniform_int_distribution<> dis(32,126); // Генерация случайных символов из ASII таблицы
 
+    for (int i = 0; i < char_count; ++i) {
+        char random_char = dis(gen);
+        cout << "Barrier thread " << thread_id << ": " << random_char << endl;
+        my_barrier.arrive_and_wait(); // Уведомляем барьер
+    }
+}
 
+int main() {
+    // Запуск потоков с mutex
+    for (int i = 0; i < thread_count; ++i) {
+        thread t(generateRandomCharsMutex, i);
+        t.join();
+    }
+
+    // Запуск потоков с semaphore
+    for (int i = 0; i < thread_count; ++i) {
+        thread t(generateRandomCharsSemaphore, i);
+        t.join();
+    }
+
+    // Запуск потоков с barrier
+    for (int i = 0; i < thread_count; ++i) {
+        thread t(generateRandomCharsBarrier, i);
+        t.join();
+    }
+
+    return 0;
 }
