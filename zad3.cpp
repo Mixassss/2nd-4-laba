7aaad55564d9489c06cd18f5dec0f4a7de1e4181
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-11-14 21:03:08 +0700

    Zadanie 3 ready

--- zad3.cpp
+++ zad3.cpp
@@ -1,80 +1,112 @@
 #include "RandW.h"
 
-WriterAndReader::WriterAndReader() : nreaders(0) {}
+WriterAndReader::WriterAndReader(bool check) : writers(0), readers(0), priority(check) {}
 
-void WriterAndReader::read(int id, int read_limit) {
-    for (int i = 0; i < read_limit; ++i) {
-        read_priority();
-        
-        cout << "Читатель " << id << " занят чтением" << endl;
-        this_thread::sleep_for(chrono::milliseconds(100));
-
-        // Завершение чтения
-        {
-            lock_guard<mutex> lock(mut_counter);
-            nreaders--;
-            if (nreaders == 0) {
-                rw_mutex.unlock(); // Последний читатель освобождает доступ писателям
-                cout << "Последний читатель " << id << " завершил чтение и освободил доступ писателям." << endl;
-            }
+void WriterAndReader::startWriting() {
+    unique_lock<mutex> lock(mx);
+    if (priority) {
+        while (writers > 0) {
+            cv.wait(lock); // Ждём других писателей
         }
-        this_thread::sleep_for(chrono::milliseconds(50));
+        writers++;
+    } else {
+        while (readers > 0 || writers > 0) {
+            cv.wait(lock); // Ждём читателей и писателей
+        }
+        writers++;
     }
 }
 
-void WriterAndReader::write(int id, int write_limit) {
-    for (int i = 0; i < write_limit; ++i) {
-        write_priority();
-        
-        cout << "Писатель " << id << " получил доступ и пишет." << endl;
-        this_thread::sleep_for(chrono::milliseconds(100));
-        
-        // Освобождаем блокировку
-        rw_mutex.unlock();
-        cout << "Писатель " << id << " завершил запись и освободил доступ." << endl;
-        
-        this_thread::sleep_for(chrono::milliseconds(50));
+void WriterAndReader::stopWriting() {
+    unique_lock<mutex> lock(mx);
+    writers--;
+    if (writers == 0) {
+        cv.notify_one(); // Уведомляем читателей
     }
 }
 
-void WriterAndReader::read_priority() {
-    lock_guard<mutex> lock(mut_counter);
-    nreaders++;
-    if (nreaders == 1) {
-        rw_mutex.lock(); // Первый читатель блокирует доступ писателям
-        cout << "Первый читатель начал чтение и заблокировал доступ для писателей." << endl;
+void WriterAndReader::startReading() {
+    unique_lock<mutex> lock(mx);
+    if (!priority) {
+        readers++;
+    } else {
+        while (writers > 0) {
+            cv.wait(lock); // Ждем всех писателей
+        }
+        readers++;
     }
 }
 
-void WriterAndReader::write_priority() {
-    rw_mutex.lock(); // Блокируем доступ всем читателям и писателям
-    cout << "Писатель заблокировал доступ для всех читателей и писателей." << endl;
+void WriterAndReader::stopReading() {
+    unique_lock<mutex> lock(mx);
+    readers--;
+    cv.notify_all(); // Уведомляем писателей
+}
+
+void WriterAndReader::replacePriority(bool change) {
+    priority = change;
+}
+
+void priority_writer(WriterAndReader& raw, int id, int iterations) { // Приоритет писателя
+    for (int i = 0; i < iterations; ++i) {
+        raw.startWriting();
+        cout << "Писатель " << id << " пишет" << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));
+        raw.stopWriting();
+    }
+}
+
+void priority_reader(WriterAndReader& raw, int id, int iterations) { // Приоритет читателя
+    for (int i = 0; i < iterations; ++i) {
+        raw.startReading();
+        cout << "Читатель " << id << " занят читением" << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));
+        raw.stopReading();
+    }
 }
 
 int main() {
-    WriterAndReader raw;
 
-    thread readers[2];
-    thread writers[2];
-
-    int read_limit = 3;  // Количество чтений для каждого читателя
+    WriterAndReader raw(true);
+    int iterations = 1;
     int write_limit = 2; // Количество записей для каждого писателя
+    int read_limit = 2; // Количество чтений для каждого читателя
 
-    for (int i = 0; i < 2; i++) { // Создание потоков читателей
-        readers[i] = thread(&WriterAndReader::read, &raw, i + 1, read_limit);
+    cout << endl << "Выбран приоритет писателя: " << endl;
+    vector<thread> threadsWriter(write_limit);
+    vector<thread> threadsReader(read_limit);
+    for (int i = 0; i < write_limit; ++i) {
+        threadsWriter[i] = thread(priority_writer, ref(raw), i+1, iterations);
     }
 
-    for (int i = 0; i < 2; i++) { // Создание потоков писателей
-        writers[i] = thread(&WriterAndReader::write, &raw, i + 1, write_limit);
+    for (int i = 0; i < read_limit; ++i) {
+        threadsReader[i] = thread(priority_reader, ref(raw), i+1, iterations);
+    }
+    for (auto& t : threadsWriter) {
+        t.join();
+    }
+    for (auto& t : threadsReader) {
+        t.join();
     }
 
-    for (int i = 0; i < 2; i++) { // Ждать завершения потоков
-        readers[i].join();
+    raw.replacePriority(false);
+
+    cout << endl << "Выбран приоритет читателя: " << endl;
+    threadsWriter.clear();
+    threadsReader.clear();
+    for (int i = 0; i < write_limit; ++i) {
+        threadsWriter[i] = thread(priority_writer, ref(raw), i + 1, iterations);
+    }
+    for (int i = 0; i < read_limit; ++i) {
+        threadsReader[i] = thread(priority_reader, ref(raw), i + 1, iterations);
+    }
+    for (auto& t : threadsWriter) {
+        t.join();
+    }
+    for (auto& t : threadsReader) {
+        t.join();
     }
 
-    for (int i = 0; i < 2; i++) { // Ждать завершения потоков писателей
-        writers[i].join();
-    }
 
     return 0;
 }
