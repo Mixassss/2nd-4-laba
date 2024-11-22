7dba7b301f9aacd48354274490f30af94eb7dcaf
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-11-22 22:35:44 +0700

    Модернизация многопотока

--- zad2.cpp
+++ zad2.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
+#include <algorithm>
 
 using namespace std;
 
@@ -26,7 +27,7 @@ private:
 
 class Maternity {
 public:
-    void addRecord() { // Метод для добавления записей о родах
+    void addRecord() { 
         int zapis;
         cout << "Введите количество записей о родах: ";
         cin >> zapis;
@@ -40,7 +41,7 @@ public:
                 int month = 1 + rand() % 12; 
                 int year = 1970 + rand() % 26;
                 string dateM = to_string(day) + '-' + to_string(month) + '-' + to_string(year);
-                if (checkDate(dateM)) { // Проверка на корректность даты
+                if (checkDate(dateM)) {
                     record.womanBirthDate = dateM;
                     break;
                 }
@@ -52,62 +53,58 @@ public:
                 int month = 1 + rand() % 12;
                 int year = 2007 + rand() % 18;
                 string dateC = to_string(day) + '-' + to_string(month) + '-' + to_string(year);
-                if (checkDate(dateC)) { // Проверка на корректность даты
+                if (checkDate(dateC)) {
                     record.babyBirthDate = dateC;
                     break;
                 }
             }
 
             // Генерация ФИО матери
-            char motherFIO = 'A' + (rand() % 26); // Генерируем случайную букву для ФИО
+            char motherFIO = 'A' + (rand() % 26); 
             record.motherFIO = string(1, motherFIO);
 
-            records.push_back(record); // Добавление записи в вектор
+            records.push_back(record);
         }
     }
 
     void calculateAverageAge(const string& startDate, const string& endDate, double& averageAge, int& count, int start = 0, int end = -1) {
-        lock_guard<mutex> lock(mx); // Блокировка мьютекса для безопасности потокa
-        if (end == -1) end = records.size(); // Установка конца диапазона
-            double totalAge = 0;
-            count = 0;
+        if (end == -1) end = records.size();
+        double totalAge = 0;
+        count = 0;
 
         for (int i = start; i < end; ++i) {
             const auto& record = records[i];
-            if (checkDate(record.babyBirthDate, startDate, endDate)) { // Проверка даты ребенка в диапазоне
-                totalAge += calculateYearDifference(record.womanBirthDate); // Суммируем возраст
+            if (checkDate(record.babyBirthDate, startDate, endDate)) {
+                totalAge += calculateYearDifference(record.womanBirthDate);
                 count++;
             }
         }
+        
         if (count > 0) {
-            averageAge = totalAge / count; // Вычисляем средний возрас
+            averageAge = totalAge / count;
         } else {
-            averageAge = 0; // Если записей нет, устанавливаем в 0
+            averageAge = 0;
         }
     }
 
-    // Метод для вывода записей о родах в заданном диапазоне дат
     void printRecordsInRange(const string& startDate, const string& endDate) {
-        lock_guard<mutex> lock(mx);
         for (const auto& record : records) {
-            if (checkDate(record.babyBirthDate, startDate, endDate)) { // Проверка даты в диапазоне
+            if (checkDate(record.babyBirthDate, startDate, endDate)) {
                 cout << "ФИО: " << record.motherFIO << " | Дата матери: " << record.womanBirthDate << " | Дата ребенка: " << record.babyBirthDate << endl;
             }
         }
     }
 
     struct MaternityRecord {
-        string womanBirthDate; // Дата рождения женщины (dd-mm-yyyy)
-        string babyBirthDate;  // Дата рождения ребенка (dd-mm-yyyy)
-        string motherFIO; // ФИО матери
+        string womanBirthDate; 
+        string babyBirthDate;  
+        string motherFIO;
     };
 
     vector<MaternityRecord> records;
 
 private:
-    mutex mx;
-
-    bool checkDate(const string& date) { // Метод для проверки корректности даты
+    bool checkDate(const string& date) { 
         stringstream ss(date);
         string token;
         getline(ss, token, '-');
@@ -121,12 +118,12 @@ private:
         if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31) return false;
         if (month == 2) {
             bool leap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-            return day <= (leap ? 29 : 28); // Проверка для февраля
+            return day <= (leap ? 29 : 28);
         }
         return true;
     }
 
-    tm stringToDate(const string& date) { // Преобразование строки в структуру даты
+    tm stringToDate(const string& date) {
         int day, month, year;
         stringstream stream(date);
         string token;
@@ -137,32 +134,31 @@ private:
         getline(stream, token, '-');
         year = stoi(token);
         tm dateStruct = {};
-        dateStruct.tm_year = year - 1900; // Настройка года
-        dateStruct.tm_mon = month - 1; // Настройка месяца
-        dateStruct.tm_mday = day; // Настройка дня
-        return dateStruct; // Возвращаем структуру даты
+        dateStruct.tm_year = year - 1900; 
+        dateStruct.tm_mon = month - 1; 
+        dateStruct.tm_mday = day; 
+        return dateStruct; 
     }
 
-    // Проверка, попадает ли дата рождения ребенка в заданный диапазон
     bool checkDate(const string& babyDate, const string& startDate, const string& endDate) {
         tm babyTm = stringToDate(babyDate);
         tm startTm = stringToDate(startDate);
         tm endTm = stringToDate(endDate);
         
-        return difftime(mktime(&babyTm), mktime(&startTm)) >= 0 && difftime(mktime(&endTm), mktime(&babyTm)) >= 0; // Проверка условий попадает ли дата в диапазон
+        return difftime(mktime(&babyTm), mktime(&startTm)) >= 0 && difftime(mktime(&endTm), mktime(&babyTm)) >= 0; 
     }
 
-    double calculateYearDifference(const string& birthDate) { // Вычисление годового разницы между датами
-        tm bDate = stringToDate(birthDate); // Преобразование строки в дату
+    double calculateYearDifference(const string& birthDate) { 
+        tm bDate = stringToDate(birthDate); 
         time_t now = time(nullptr);
         tm* nowTm = localtime(&now);
-        double age = difftime(mktime(nowTm), mktime(&bDate)) / (60 * 60 * 24 * 365.25); // Вычисление возраста
+        double age = difftime(mktime(nowTm), mktime(&bDate)) / (60 * 60 * 24 * 365.25); 
         return age;
     }
 };
 
 int main() {
-    srand(static_cast<unsigned int>(time(0))); // Инициализация генератора случайных чисел
+    srand(static_cast<unsigned int>(time(0))); 
     Maternity maternity;
 
     cout << "Выберите действие: 1 - добавить записи, 2 - вычислить средний возраст" << endl;
@@ -186,13 +182,13 @@ int main() {
                 double averageAge;
                 int count;
                 cout << "Однопоточная обработка: " << endl;
-                maternity.calculateAverageAge(startDate, endDate, averageAge, count); // Вычисление среднего возраста
+                maternity.calculateAverageAge(startDate, endDate, averageAge, count);
                 if (count > 0)
                     cout << "Средний возраст: " << averageAge << endl;
                 else
                     cout << "Записи не найдены." << endl;
 
-                maternity.printRecordsInRange(startDate, endDate); // Вывод записей в диапазоне
+                maternity.printRecordsInRange(startDate, endDate);
             }
 
             // Многопоточная обработка
@@ -200,41 +196,42 @@ int main() {
             cout << "Введите количество потоков: ";
             cin >> countThreads;
 
-            int size = maternity.records.size(); // Размер вектора записей
-            int chunkSize = (size + countThreads - 1) / countThreads; // Размер блока для каждого потока
-            vector<thread> threads(countThreads); // Вектор потоков
-            vector<double> averages(countThreads, 0); // Вектор для хранения средних возрастов
+            int size = maternity.records.size(); 
+            int chunkSize = (size + countThreads - 1) / countThreads; 
+            vector<thread> threads(countThreads); 
+            vector<double> averages(countThreads, 0); 
             vector<int> counts(countThreads, 0);
 
-            {
-                Timer t;
-                cout << "Многопоточная обработка: " << endl;
-                for (int i = 0; i < countThreads; ++i) {
-                    int start = i * chunkSize;
-                    int end = min(start + chunkSize, size); // Обработка последнего потока
+            Timer t;
+            cout << "Многопоточная обработка: " << endl;
+            for (int i = 0; i < countThreads; ++i) {
+                int start = i * chunkSize;
+                int end = min(start + chunkSize, size);
+                if (start < size) { // Проверка, чтобы избежать пустых потоков
                     threads[i] = thread([&maternity, &startDate, &endDate, i, start, end, &averages, &counts]() {
-                        maternity.calculateAverageAge(startDate, endDate, averages[i], counts[i], start, end); // Вызов вычислений в потоке
+                        maternity.calculateAverageAge(startDate, endDate, averages[i], counts[i], start, end);
                     });
                 }
-                for (int i = 0; i < countThreads; ++i) {
+            }
+            for (int i = 0; i < countThreads; ++i) {
+                if (threads[i].joinable()) {
                     threads[i].join();
                 }
-
-                // Считаем общий средний возраст
-                double totalAge = 0;
-                int totalCount = 0;
-                for (int i = 0; i < countThreads; ++i) {
-                    totalAge += averages[i] * counts[i]; // Суммирование возрастов
-                    totalCount += counts[i]; // Суммирование количества записей
-                }
-                if (totalCount > 0)
-                    cout << "Средний возраст: " << totalAge / totalCount << endl;
-                else
-                    cout << "Записи не найдены." << endl;
-
-                maternity.printRecordsInRange(startDate, endDate);
-                break;
             }
+
+            double totalAge = 0;
+            int totalCount = 0;
+            for (int i = 0; i < countThreads; ++i) {
+                totalAge += averages[i] * counts[i];
+                totalCount += counts[i];
+            }
+            if (totalCount > 0)
+                cout << "Средний возраст: " << totalAge / totalCount << endl;
+            else
+                cout << "Записи не найдены." << endl;
+
+            maternity.printRecordsInRange(startDate, endDate);
+            break;
         }
     }
     return 0;
